8259f5ada8a36a9f41baae1b3973368b195413ec
Erik Malm <erik.malm@etimo.se>
2017-12-27 01:38:30 +0100

    Tweaks to work with scraper

---
+++ assets/blogAssets/F84JJ98TX--.cs
@@ -0,0 +1,12 @@
+Starting test execution, please wait...
+[xUnit.net 00:00:00.4044360]   Discovering: test
+[xUnit.net 00:00:00.4875050]   Discovered:  test
+[xUnit.net 00:00:00.5306630]   Starting:    test
+[xUnit.net 00:00:00.6852600]     test.UnitTest1.DividableByFiveIsBuzz [FAIL]
+[xUnit.net 00:00:00.6866630]       Assert.Equal() Failure
+[xUnit.net 00:00:00.6867430]                 ↓ (pos 0)
+[xUnit.net 00:00:00.6867830]       Expected: Buzz
+[xUnit.net 00:00:00.6868770]       Actual:   0
+[xUnit.net 00:00:00.6869530]                 ↑ (pos 0)
+[xUnit.net 00:00:00.6879490]       Stack Trace:
+[xUnit.net 00:00:00.6891370]         /Users/jens-peter.olsson/Documents/code/fizzbuzz/test/UnitTest1.cs(25,0): at test.UnitTest1.DividableByFiveIsBuzz()
